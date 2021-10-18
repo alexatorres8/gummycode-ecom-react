28ce5d3c01223f84021f221281880e3a2c5d6095
alexatorres8 <ajt8@live.com>
2021-10-18 11:11:02 -0400

    built out app grid and header and navbar components

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import Header from './navbarheader/header';
+import Navbar from './navbarheader/navbar';
+
 class App extends Component {
   render() {
     return (
       <div className='app'>
+        ART
         {this.props.children}
+        <Header />
+        <Navbar />
       </div>
     );
   }
