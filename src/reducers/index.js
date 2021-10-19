28f49429bfba8a4427f049ff77d991bb6f3efa65
alexatorres8 <ajt8@live.com>
2021-10-19 16:16:49 -0400

    built navbarheader reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
+import NavbarHeader from './navbarheaderReducer';
+
 const rootReducer = combineReducers({
-  form
+  form,
+  NavbarHeader
 });
 
 export default rootReducer;
