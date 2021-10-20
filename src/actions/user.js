8b2c6ff1ffea4417af6b2dc4353cd6220a3e66a4
alexatorres8 <ajt8@live.com>
2021-10-20 17:33:03 -0400

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,13 +8,47 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02 
+                total: 19.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 19.40 
+                total: 9.40,
+                orderNumber: 'B0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Max Hudgens',
+                    shippingAddress: '456 West State Street'
+                }
+            },
+            {
+                _id: 2,
+                total: 1.40,
+                orderNumber: 'C0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Tiffany Hudgens',
+                    shippingAddress: '789 West State Street'
+                }
+            },
+            {
+                _id: 3,
+                total: 2.40,
+                orderNumber: 'D0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Kristine Hudgens',
+                    shippingAddress: '1011 West State Street'
+                }
             }
         ]
     })
