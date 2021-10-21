01bc9a0f4a06b2f9911aa122a61a6437ba42bef5
alexatorres8 <ajt8@live.com>
2021-10-20 20:52:28 -0400

    developed shopReducer and fetch shop categories

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { reducer as form } from 'redux-form';
 
 import navbarHeader from './navbarheaderReducer';
 import user from './userReducer';
+import shop from './shopReducer';
 
 const rootReducer = combineReducers({
   form,
   navbarHeader,
-  user
+  user,
+  shop
 });
 
 export default rootReducer;
